e181cc0cd190bf178f8535eea10eaed5938c364c
Hibiki <theunpleasantowl@gmail.com>
2019-04-01 20:12:51 -0400

    Implement CreateProcess

--- simulator.c
+++ simulator.c
@@ -789,39 +789,37 @@ void DumpMemory(
 long CreateProcess (char* filename, long priority)
 {
 	// Allocate space for Process Control Block
-	long *PCBptr = &mem[MAX_HEAP_MEMORY + 1]; //TODO: Wrong behavior,
+	long *PCBptr = &EndOfList; //TODO: Wrong behavior,
 
 	// Initialize PCB: Set nextPCBlink to end of list, default priority, Ready state, and PID
-	//InitializePCB(PCBptr); //TODO: Enable once function implemented
+	InitializePCB(PCBptr); //TODO: FIX names once function implemented
 
 	// Load the program
-	Set value =load the program calling by Absolute Loader passing filename as argument;
-	Check for error and return error code, if loading program failed
+	if (AbsoluteLoader(filename) == OK)
+		pcb->pc = pc; 		// Store PC value in the PCB of the process
+	else
+		return ErrorFileOpen;
 
-		// store PC value in the PCB of the process
-		Set PC value in the PCB = value;
-
-	// Allocate stack space from user free list
-	Set ptr = Allocate User Memory of size StackSize;
-	if (ptr < 0)	// check for error
-	{  // User memory allocation failed
-		Free allocated PCB space by calling Free OS Memory passing PCBptr and  PCBsize;
-		return(ptr);  // return error code
+					// Allocate stack space from user free list
+	pcb->ptr = StackSize; 		// Set ptr = Allocate User Memory of size StackSize;
+	if (ptr < 0)			// Check for error
+	{  				// User memory allocation failed
+		FreeOSMemory(PCBptr, SIZE);
+		return(ErrorInvalidMemorySize);  		// return error code
 	}
 
 	// Store stack information in the PCB – SP, ptr, and size
-	Set SP in the PCB = ptr + Stack Size;  // empty stack is high address, full is low address
-	Set stack start address in the PCB to ptr;
-	Set stack size in the PCB = Stack Size;
+	pcb->sp           = ptr + SIZE;		// empty stack is high address, full is low address
+	pcb->StartAddress = ptr;
+	pcb->StackSize    = SIZE;
+	pcb->priority     = DEFAULT_PRIORITY;	// Set priority
 
-	Set priority in the PCB = priority;	// Set priority
+	DumpMemory("PCB Created", ptr, SIZE);				// Dump PCB stack
 
-	Dump program area;
-
-	Print PCB passing PCBptr;
+	//TODO: print PCB
 
 	// Insert PCB into Ready Queue according to the scheduling algorithm
-	Insert PCB into Ready Queue passing PCBptr;
+	InsertIntoRQ(PCBptr);
 
 	return(OK);
 }
